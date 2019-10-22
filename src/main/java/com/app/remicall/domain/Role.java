00efd779ad23542b3e77100255c53d33bbef4714
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-10-22 21:45:56 +0400

    webapp-remicall add Roles

--- src/main/java/com/app/remicall/domain/Role.java
+++ src/main/java/com/app/remicall/domain/Role.java
@@ -1,9 +1,11 @@
 package com.app.remicall.domain;
 
+import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
 
+@ToString
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
