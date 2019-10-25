c05fe1114ea534e76c887afa04203562cbfc5459
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-10-25 23:33:15 +0400

    webapp-remicall in progress(2) uploads system

--- src/main/java/com/app/remicall/domain/Message.java
+++ src/main/java/com/app/remicall/domain/Message.java
@@ -23,6 +23,8 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
+    private String filename;
+
     public Message(String text, String tag, User author) {
         this.text = text;
         this.tag = tag;
