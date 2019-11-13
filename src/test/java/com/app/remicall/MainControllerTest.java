b1634046e08c9ad20681860e6f7dad2a0688cfeb
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-11-13 23:58:28 +0300

    webapp-remicall in progress tests

--- src/test/java/com/app/remicall/MainControllerTest.java
+++ src/test/java/com/app/remicall/MainControllerTest.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcSecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -20,6 +20,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @WithUserDetails("user")
+@TestPropertySource("/application-test.properties")
+@Sql(value = {"/testdb/create_user_before.sql"},
+    executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/testdb/remove_user_after.sql"},
+    executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class MainControllerTest {
 
     @Autowired
@@ -33,5 +38,13 @@ public class MainControllerTest {
                 .andExpect(authenticated())
                 .andExpect(xpath("//div[@id='navbarSupportedContent']/div")
                         .string("user"));
+    }
+
+    @Test
+    public void messageListTest() throws Exception {
+        this.mock.perform(get("/main"))
+                .andDo(print())
+                .andExpect(authenticated())
+                .andExpect(xpath("").nodeCount(0));
     }
 }
